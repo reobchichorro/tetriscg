cbb43d2309a91198b39a04c5aff2e67a23be1b13
Rodrigo Chichorro <reobcnegf@gmail.com>
2019-10-05 18:28:36 -0300

    Desenha corretamente o menu

--- Menu.cpp
+++ Menu.cpp
@@ -17,12 +17,105 @@ struct Menu {
 
 Menu omenu;
 
+void DesenhaRet(void) {
+
+    // Define a cor corrente
+    glColor3f(0.0,1.0,0.0);
+
+    GLint l=100, h=40, ldiff=10, hdiff=20;
+    GLint ll3 = (3*l + 2*ldiff)/2, ll2 = (2*l + ldiff)/2;
+    glBegin(GL_POLYGON);
+        glVertex2i(-1*l/2, hdiff/2 + 2*hdiff + 2*h);
+        glVertex2i(-1*l/2, hdiff/2 + 2*hdiff + 3*h);
+        glVertex2i(l/2, hdiff/2 + 2*hdiff + 3*h);
+        glVertex2i(l/2, hdiff/2 + 2*hdiff + 2*h);
+    glEnd();
+    for(int i=0; i<3; i++) {
+        if(omenu.coluna[1]==i)
+            glColor3f(0.0,0.0,1.0);
+        else
+            glColor3f(0.0,1.0,0.0);
+        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
+        glBegin(GL_POLYGON);
+            glVertex2i(esq, hdiff + hdiff/2 + h);
+            glVertex2i(esq, hdiff + hdiff/2 + 2*h);
+            glVertex2i(dir, hdiff + hdiff/2 + 2*h);
+            glVertex2i(dir, hdiff + hdiff/2 + h);
+        glEnd();
+    }
+    for(int i=0; i<3; i++) {
+        if(omenu.coluna[2]==i)
+            glColor3f(0.0,0.0,1.0);
+        else
+            glColor3f(0.0,1.0,0.0);
+        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
+        glBegin(GL_POLYGON);
+            glVertex2i(esq, hdiff/2);
+            glVertex2i(esq, hdiff/2 + h);
+            glVertex2i(dir, hdiff/2 + h);
+            glVertex2i(dir, hdiff/2);
+        glEnd();
+    }
+    for(int i=0; i<3; i++) {
+        if(omenu.coluna[3]==i)
+            glColor3f(0.0,0.0,1.0);
+        else
+            glColor3f(0.0,1.0,0.0);
+        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
+        glBegin(GL_POLYGON);
+            glVertex2i(esq, -1*hdiff/2 - h);
+            glVertex2i(esq, -1*hdiff/2);
+            glVertex2i(dir, -1*hdiff/2);
+            glVertex2i(dir, -1*hdiff/2 - h);
+        glEnd();
+    }
+    for(int i=0; i<2; i++) {
+        if(omenu.coluna[4]==i)
+            glColor3f(0.0,0.0,1.0);
+        else
+            glColor3f(0.0,1.0,0.0);
+        GLint esq = -1*ll2 + i*(ldiff+l), dir = esq + l;
+        glBegin(GL_POLYGON);
+            glVertex2i(esq, -1*hdiff/2 - hdiff - 2*h);
+            glVertex2i(esq, -1*hdiff/2 - hdiff - h);
+            glVertex2i(dir, -1*hdiff/2 - hdiff - h);
+            glVertex2i(dir, -1*hdiff/2 - hdiff - 2*h);
+        glEnd();
+    }
+    glColor3f(0.0,1.0,0.0);
+    glBegin(GL_POLYGON);
+        glVertex2i(-1*l/2, -1*hdiff/2 - 2*hdiff - 3*h);
+        glVertex2i(-1*l/2, -1*hdiff/2 - 2*hdiff - 2*h);
+        glVertex2i(l/2, -1*hdiff/2 - 2*hdiff - 2*h);
+        glVertex2i(l/2, -1*hdiff/2 - 2*hdiff - 3*h);
+    glEnd();
+
+
+    glColor3f(1.0,0.0,0.0);
+    glLineWidth(2.0);
+    GLint baixo = hdiff/2 - 2 + (2-omenu.linha)*(hdiff+h), cima = baixo + h + 4;
+    GLint esq, dir;
+    if(omenu.linha > 0 && omenu.linha < 4) {
+        esq = -1*ll3 + omenu.coluna[omenu.linha]*(ldiff+l) - 2, dir = esq + l + 2;
+    } else if(omenu.linha == 4) {
+        esq = -1*ll2 + omenu.coluna[4]*(ldiff+l) - 2, dir = esq + l + 2;
+    } else {
+        dir = l/2 + 2; esq = -1*dir;
+    }
+    glBegin(GL_LINE_LOOP);
+        glVertex2i(esq, baixo);
+        glVertex2i(esq, cima);
+        glVertex2i(dir, cima);
+        glVertex2i(dir, baixo);
+    glEnd();
+}
+
 //funções de tratamento das opções do menu deverão estar aqui
 void opcao_menu(int opcao, int x, int y)
 {
     switch(opcao)
     {
-        case 8 : //sair
+        case 8: //sair
             exit(0);
         break;
         case GLUT_KEY_LEFT:
@@ -46,67 +139,9 @@ void opcao_menu(int opcao, int x, int y)
                 omenu.linha=0;
         break;
     }
+    glutPostRedisplay();
 }
 
-void DesenhaRet(void) {
-
-    // glBegin(GL_POLYGON);
-    //         glVertex2f(-160.0f, -180.0f);
-    //         glVertex2f(-160.0f, 180.0f);
-    //         glVertex2f(160.0f, 180.0f);
-    //         glVertex2f(160.0f, -180.0f);
-    //  glEnd();
-    GLint l=100, h=40, ldiff=10, hdiff=20;
-    GLint ll3 = (3*l + 2*ldiff)/2, ll2 = (2*l + ldiff)/2;
-    glBegin(GL_POLYGON);
-        glVertex2i(-1*l/2, hdiff/2 + 2*hdiff + 3*h);
-        glVertex2i(-1*l/2, hdiff/2 + 2*hdiff + 2*h);
-        glVertex2i(l/2, hdiff/2 + 2*hdiff + 2*h);
-        glVertex2i(l/2, hdiff/2 + 2*hdiff + 3*h);
-    glEnd();
-    for(int i=0; i<3; i++) {
-        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
-        glBegin(GL_POLYGON);
-            glVertex2i(esq, hdiff + hdiff/2 + h);
-            glVertex2i(esq, hdiff + hdiff/2 + 2*h);
-            glVertex2i(dir, hdiff + hdiff/2 + 2*h);
-            glVertex2i(dir, hdiff + hdiff/2 + h);
-        glEnd();
-    }
-    for(int i=0; i<3; i++) {
-        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
-        glBegin(GL_POLYGON);
-            glVertex2i(esq, hdiff/2);
-            glVertex2i(esq, hdiff/2 + h);
-            glVertex2i(dir, hdiff/2 + h);
-            glVertex2i(dir, hdiff/2);
-        glEnd();
-    }
-    for(int i=0; i<3; i++) {
-        GLint esq = -1*ll3 + i*(ldiff+l), dir = esq + l;
-        glBegin(GL_POLYGON);
-            glVertex2i(esq, -1*hdiff/2);
-            glVertex2i(esq, -1*hdiff/2 - h);
-            glVertex2i(dir, -1*hdiff/2 - h);
-            glVertex2i(dir, -1*hdiff/2);
-        glEnd();
-    }
-    for(int i=0; i<2; i++) {
-        GLint esq = -1*ll2 + i*(ldiff+l), dir = esq + l;
-        glBegin(GL_POLYGON);
-            glVertex2i(esq, -1*hdiff/2 - hdiff - 2*h);
-            glVertex2i(esq, -1*hdiff/2 - hdiff - h);
-            glVertex2i(dir, -1*hdiff/2 - hdiff - h);
-            glVertex2i(dir, -1*hdiff/2 - hdiff - 2*h);
-        glEnd();
-    }
-    glBegin(GL_POLYGON);
-        glVertex2i(-1*l/2, -1*hdiff/2 - 2*hdiff - 3*h);
-        glVertex2i(-1*l/2, -1*hdiff/2 - 2*hdiff - 2*h);
-        glVertex2i(l/2, -1*hdiff/2 - 2*hdiff - 2*h);
-        glVertex2i(l/2, -1*hdiff/2 - 2*hdiff - 3*h);
-    glEnd();
-}
 // Desenha um texto na janela GLUT
 void DesenhaTexto(char *string)
 {
@@ -127,8 +162,6 @@ void Desenha(void)
 
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Define a cor corrente
-    glColor3f(0.0,1.0,1.0);
     DesenhaRet();
 
     // Exibe a posi��o do mouse na janela
@@ -137,34 +170,6 @@ void Desenha(void)
 
     glutSwapBuffers();
 }
-
-// Gerenciamento do menu principal
-void MenuPrincipal(int op)
-{
-}
-
-// // Criacao do Menu
-// void CriaMenu()
-// {
-//     // int menu,submenu1,submenu2;
-
-//     // submenu1 = glutCreateMenu();
-//     // glutAddMenuEntry("Vermelho",0);
-//     // glutAddMenuEntry("Verde",1);
-//     // glutAddMenuEntry("Azul",2);
-
-//     // submenu2 = glutCreateMenu();
-//     // glutAddMenuEntry("Quadrado",0);
-//     // glutAddMenuEntry("Tri�ngulo",1);
-//     // glutAddMenuEntry("Losango",2);
-
-//     // menu = glutCreateMenu();
-//     // glutAddSubMenu("Cor",submenu1);
-//     // glutAddSubMenu("Primitivas",submenu2);
-//     DesenhaRet();
-
-//     glFlush();
-// }
 
 // Inicializa par�metros de rendering
 void Inicializa (void)
@@ -216,15 +221,15 @@ void ChangeSize(GLsizei w, GLsizei h)
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(500,500);
-    glutInitWindowPosition(10,10);
-    glutCreateWindow("Exemplo de Menu");
-    glutDisplayFunc(Desenha);
-    glutReshapeFunc(ChangeSize);
-    glutMotionFunc(MoveMouseBotaoPressionado);
-    glutPassiveMotionFunc(MoveMouse);
-    glutSpecialFunc(opcao_menu);
-    Inicializa();
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+        glutInitWindowSize(500,500);
+        glutInitWindowPosition(10,10);
+        glutCreateWindow("Exemplo de Menu");
+        glutDisplayFunc(Desenha);
+        glutReshapeFunc(ChangeSize);
+        glutMotionFunc(MoveMouseBotaoPressionado);
+        glutPassiveMotionFunc(MoveMouse);
+        glutSpecialFunc(opcao_menu);
+        Inicializa();
     glutMainLoop();
 }
