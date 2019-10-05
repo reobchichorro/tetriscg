c6a1909282ac8d556fc2176fe21320f9c6f67a1d
Rodrigo Chichorro <reobcnegf@gmail.com>
2019-10-05 19:20:51 -0300

    Menu aceita mouse

--- Menu.cpp
+++ Menu.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include <string>
 
-
-char texto[30];
+std::string texto;
 GLfloat win;
 GLint view_w, view_h;
+
+GLint l=100, h=40, ldiff=10, hdiff=20;
+GLint ll3 = (3*l + 2*ldiff)/2, ll2 = (2*l + ldiff)/2;
 
 //estrutura de dados que repsenta o menu
 struct Menu {
@@ -21,9 +24,6 @@ void DesenhaRet(void) {
 
     // Define a cor corrente
     glColor3f(0.0,1.0,0.0);
-
-    GLint l=100, h=40, ldiff=10, hdiff=20;
-    GLint ll3 = (3*l + 2*ldiff)/2, ll2 = (2*l + ldiff)/2;
 
     GLint baixo, cima;
     GLint esq, dir;
@@ -53,7 +53,6 @@ void DesenhaRet(void) {
     glColor3f(1.0,0.0,0.0);
     glLineWidth(2.0);
     baixo = hdiff/2 - 2 + (2-omenu.linha)*(hdiff+h), cima = baixo + h + 4;
-    // GLint esq, dir;
     if(omenu.linha > 0 && omenu.linha < 4) {
         esq = -1*ll3 + omenu.coluna[omenu.linha]*(ldiff+l) - 2; dir = esq + l + 4;
     } else if(omenu.linha == 4) {
@@ -70,8 +69,7 @@ void DesenhaRet(void) {
 }
 
 //funções de tratamento das opções do menu deverão estar aqui
-void opcao_menu(int opcao, int x, int y)
-{
+void opcao_menu(int opcao, int x, int y) {
     switch(opcao)
     {
         case GLUT_KEY_LEFT:
@@ -98,8 +96,7 @@ void opcao_menu(int opcao, int x, int y)
     glutPostRedisplay();
 }
 
-void opcao_menu2(unsigned char opcao, int x, int y)
-{
+void opcao_menu2(unsigned char opcao, int x, int y) {
     switch(opcao)
     {
         case 13: //sair
@@ -113,14 +110,15 @@ void opcao_menu2(unsigned char opcao, int x, int y)
 }
 
 // Desenha um texto na janela GLUT
-void DesenhaTexto(char *string)
+void DesenhaTexto(std::string& texto)
 {
+    auto it = texto.begin();
   	glPushMatrix();
         // Posi��o no universo onde o texto ser� colocado
         glRasterPos2f(-win,win-(win*0.08));
         // Exibe caracter a caracter
-        while(*string)
-             glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10,*string++);
+        while(it != texto.end())
+             glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10,*it++);
 	glPopMatrix();
 }
 
@@ -147,7 +145,7 @@ void Inicializa (void)
     // Define a cor de fundo da janela de visualiza��o como branca
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     win=250.0f;
-    strcpy(texto, "(0,0)");
+    texto = "(0,0)";
 
     // CriaMenu();
 
@@ -155,9 +153,50 @@ void Inicializa (void)
 
 // Fun��o callback chamada sempre que o mouse � movimentado
 // sobre a janela GLUT com um bot�o pressionado
-void MoveMouseBotaoPressionado(int x, int y)
+void MoveMouseBotaoPressionado(int button, int state, int x, int y)
 {
-    sprintf(texto, "Botao pressionado (%d,%d)", x, y);
+    if(button != GLUT_LEFT_BUTTON) return;
+    texto = "Botao pressionado (" + std::to_string(x) + "," + std::to_string(y) + ")";
+
+    x -= 250; y = 250 - y;
+
+    GLint baixo, cima;
+    GLint esq, dir;
+    bool found = false;
+    for(int i=0; i<6 && !found; i++) {
+        baixo = hdiff/2 + (2-i)*(hdiff+h), cima = baixo + h;
+        for(int j=0; j<omenu.maxMenu[i] && !found; j++) {
+            if(i > 0 && i < 4) {
+                esq = -1*ll3 + j*(ldiff+l); dir = esq + l;
+            } else if(i == 4) {
+                esq = -1*ll2 + j*(ldiff+l); dir = esq + l;
+            } else {
+                dir = l/2; esq = -1*dir;
+            }
+            if(esq <= x && x <= dir && baixo <= y && y <= cima) {
+                found = true;
+                if(i==5)
+                    exit(0);
+                // else if(i==0)
+                    //iniciar jogo
+                else {
+                    omenu.linha = i;
+                    omenu.coluna[i] = j;
+                }
+            }
+            // if(omenu.coluna[i]==j && (i > 0 && i < 5))
+            //     glColor3f(0.0,0.0,1.0);
+            // else
+            //     glColor3f(0.0,1.0,0.0);
+            // glBegin(GL_POLYGON);
+            //     glVertex2i(esq, baixo);
+            //     glVertex2i(esq, cima);
+            //     glVertex2i(dir, cima);
+            //     glVertex2i(dir, baixo);
+            // glEnd();
+        }
+    }
+
     glutPostRedisplay();
 }
 
@@ -165,7 +204,7 @@ void MoveMouseBotaoPressionado(int x, int y)
 // sobre a janela GLUT
 void MoveMouse(int x, int y)
 {
-    sprintf(texto, "(%d,%d)", x, y);
+    texto = "(" + std::to_string(x) + "," + std::to_string(y) + ")";
     glutPostRedisplay();
 }
 
@@ -197,7 +236,7 @@ int main(int argc, char** argv)
         glutCreateWindow("Exemplo de Menu");
         glutDisplayFunc(Desenha);
         glutReshapeFunc(ChangeSize);
-        glutMotionFunc(MoveMouseBotaoPressionado);
+        glutMouseFunc(MoveMouseBotaoPressionado);
         glutPassiveMotionFunc(MoveMouse);
         glutSpecialFunc(opcao_menu);
         glutKeyboardFunc(opcao_menu2);
